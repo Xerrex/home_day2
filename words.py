47bf59151b052646c9f322954a06f9509d2a3039
Xerrex <alexkagai@outlook.com>
2017-06-14 13:52:27 +0300

    changed words function to pass all tests

--- words.py
+++ words.py
@@ -6,11 +6,21 @@ def words(word):
     else:
         wordlist = word.split()
 
-    print("\n\nworld list after splitting ", wordlist,"\n\n")
+    print("\nworld list after splitting ", wordlist,"\n")
 
     for i_word in wordlist:
-        wordcount[i_word]=wordlist.count(i_word)
+        try:
+            key=int(i_word)
+            wordcount[key] = wordlist.count(i_word)
+
+        except ValueError:
+            wordcount[i_word] = wordlist.count(i_word)
+
 
     return wordcount
 
 
+
+
+print("from the My function ",words('testing 1 2 testing'))
+print("from the Tests       ",{'testing': 2, 1: 1, 2: 1})
